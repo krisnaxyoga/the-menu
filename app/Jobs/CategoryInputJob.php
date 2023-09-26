e1fa446af1cae3cd287225b89a1e7873605f4a70
muliadev3 <softhouse3@themulia.com>
2023-09-27 07:51:42 +0800

    cronjob

---
+++ app/Jobs/CategoryInputJob.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use App\Models\CategoryProduct;
+
+class CategoryInputJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $newCategory = new CategoryProduct();
+        $newCategory->name = 'New Category'; // Ganti dengan nama kategori yang sesuai
+        $newCategory->save();
+    }
+}
--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -6,7 +6,9 @@
 
         <!-- Outer Row -->
 
-                <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card o-hidden border-0 shadow-lg my-5">  
+
+                
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
